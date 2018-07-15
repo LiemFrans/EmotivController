81b06c4a710176ed6cabf5e092a0c2470d5bd6a0
LiemFrans <fransiskus.cahyadi@gmail.com>
2018-07-15 19:59:58 +0700

    UI
    Update UI

--- EmotivController/FormNavigation.Designer.cs
+++ EmotivController/FormNavigation.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EmotivController
 {
-    partial class Form1
+    partial class FormNavigation
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormNavigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 512);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormNavigation";
+            this.Text = "Form Navigation";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ EmotivController/FormNavigation.cs
@@ -0,0 +1,23 @@
+﻿using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmotivController
+{
+    public partial class FormNavigation : Form
+    {
+        public FormNavigation()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
